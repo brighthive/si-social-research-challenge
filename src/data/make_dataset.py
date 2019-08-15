573b6ea69501234bc90843b80cf4b4560e958608
Kwame Robinson <kporterrobinson@gmail.com>
2019-08-15 18:56:26 -0400

    (WIP) setting up make_dataset.py for the prospective candidate, note I would like to add a nifty progress bar but let's get the bulk of the actual challenge done first

---
+++ src/data/make_dataset.py
@@ -0,0 +1,38 @@
+import boto3
+import progressbar
+from pathlib import Path
+from yaml import safe_load as load
+with open("config.yaml") as obj:
+    config = load(obj)
+
+# todo: add nifty progress bar
+# see: https://stackoverflow.com/questions/41827963/track-download-progress-of-s3-file-using-boto3-and-callbacks
+
+s3 = boto3.client('s3', region_name="us-east-2")
+
+def get_bucket_name(config=config):
+    return config['s3']['bucket']
+
+def get_external_data_config(key="external_data", config=config):
+    return config[key]
+
+def download_from_s3_and_save(s3_obj=s3,
+                              file_path="./data/external/",
+                              file_name=None):
+    file_name = Path(file_name)
+    save_to_path = Path(file_path)
+    save_to_path.mkdir(
+        parents=True,
+        exist_ok=True
+    )
+
+    s3.download_file(get_bucket_name(),
+                     str(file_name),
+                     str(save_to_path/file_name))
+
+def download_external_data_and_save(data=None):
+    external_config = get_external_data_config()
+    download_from_s3_and_save(file_name=external_config[data])
+
+download_external_data_and_save("adult_training_and_education")
+print("we're done")
